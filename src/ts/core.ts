b98531b3aedf0ebce6bf4af1506a743aef7f8290
akosnad <akos.nadudvari@pinkcloud.hu>
2019-06-13 21:21:29 +0200

    Added 3d logo

--- src/ts/core.ts
+++ src/ts/core.ts
@@ -1,4 +1,9 @@
-import * as Babylon from 'babylonjs';
+import * as Babylon from 'babylonjs'
+import * as BabylonLoaders from 'babylonjs-loaders'
+import { Vector3 } from 'babylonjs'
+import * as BabylonMaterials from 'babylonjs-materials'
+import * as BabylonTextures from 'babylonjs-procedural-textures'
+
 
 namespace ThreeDeeTinker {
 
@@ -6,7 +11,7 @@ namespace ThreeDeeTinker {
         private canvas: HTMLCanvasElement
         private engine: Babylon.Engine
         private scene: Babylon.Scene
-        private camera: Babylon.Camera
+        private camera: Babylon.ArcRotateCamera
         private light: Babylon.Light
 
         constructor(canvas: HTMLCanvasElement) {
@@ -15,13 +20,14 @@ namespace ThreeDeeTinker {
 
             this.scene = new Babylon.Scene(this.engine)
 
-            this.camera = new Babylon.UniversalCamera('camera1', new Babylon.Vector3(0, 5, -10), this.scene)
+            this.camera = new Babylon.ArcRotateCamera('camera1', 0, 0.8, 100, Babylon.Vector3.Zero(), this.scene)
             this.camera.attachControl(this.canvas, false)
             this.camera.inertia = 0.8
 
-            this.light = new Babylon.HemisphericLight('light1', new Babylon.Vector3(0, 1, 0), this.scene)
+            this.light = new Babylon.HemisphericLight('light1', new Babylon.Vector3(0.5, 1, 0.5), this.scene)
 
             let sphere = Babylon.MeshBuilder.CreateSphere('sphere1', { segments: 16, diameter: 2 }, this.scene)
+            sphere.position.x = -5
             sphere.position.y = 1
 
             let ground = Babylon.MeshBuilder.CreateGround('ground', { width: 6, height: 6, subdivisions: 2 }, this.scene)
@@ -29,6 +35,23 @@ namespace ThreeDeeTinker {
             let cube = Babylon.MeshBuilder.CreateBox('box1', { size: 2 }, this.scene)
             cube.position.x = 5
             cube.position.y = 1
+
+            BabylonLoaders.OBJFileLoader.INVERT_Y = false
+            // BabylonLoaders.OBJFileLoader.UV_SCALING = new Babylon.Vector2(100, 100)
+            // BabylonLoaders.OBJFileLoader.IMPORT_VERTEX_COLORS = true
+            Babylon.SceneLoader.LoadAssetContainer('/', "pinkcloud_3d.obj", this.scene, (container) => {
+                let meshes = container.meshes
+                // let materials = container.materials
+
+                for (const mesh of meshes) {
+                    mesh.scaling = new Vector3(15, 15, 15)
+                    mesh.translate(new Vector3(0, 1, 0), 0.05)
+                    mesh.rotate(new Vector3(0, 0, 1), Math.PI)
+                    // mesh.translate(new Vector3(1, 0, 0), -0.05)
+                }
+
+                container.addAllToScene()
+            })
         }
         render(): void {
             this.engine.runRenderLoop(() => {
